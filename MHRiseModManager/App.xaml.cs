72563dae8cff36d7612a63b985c018d6d682a03b
Ruhiel <ruhiel1983@gmail.com>
2022-09-05 20:47:55 +0900

    設定画面デザイン修正＆二重起動防止

--- MHRiseModManager/App.xaml.cs
+++ MHRiseModManager/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Threading;
 
 namespace MHRiseModManager
 {
@@ -13,5 +14,28 @@ namespace MHRiseModManager
     /// </summary>
     public partial class App : Application
     {
+        private Mutex _mutex = new Mutex(false, "MHRiseModManager");
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if(_mutex.WaitOne(0, false))
+            {
+                return;
+            }
+
+            MessageBox.Show("二重起動できません", "情報", MessageBoxButton.OK, MessageBoxImage.Information);
+            _mutex.Close();
+            _mutex = null;
+            Shutdown();
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            if(_mutex != null)
+            {
+                _mutex.ReleaseMutex();
+                _mutex.Close();
+            }
+        }
     }
 }
